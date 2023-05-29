3fc4574f07725ab97503fa262040a62605ba2678
Blueaulo <qualcosa588@gmail.com>
2023-05-29 19:18:23 +0200

    a lot of polishing

--- Articoli2.py
+++ Articoli2.py
@@ -1,10 +1,18 @@
-from bs4 import BeautifulSoup 
+import re
+from bs4 import BeautifulSoup
 import requests
-url = input("Put your link here: ")
-source = requests.get(url)
-soup = BeautifulSoup(source.text, "lxml")
-raw_text = soup.find_all('p')
-print ('-------------------')
-for article in raw_text:
-    print(article.text)
-input("Press enter to close program")
+
+url = input("Enter the URL of the webpage you want to scrape: ")
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "lxml")
+paragraphs = soup.find_all('p')
+
+print('-------------------')
+for paragraph in paragraphs:
+    text = paragraph.text.strip()
+    if text:
+        formatted_text = re.sub(r'\.(\s|$)', '.\n', text)
+        print(formatted_text)
+
+input("Press enter to close the program")
